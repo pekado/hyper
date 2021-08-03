e1339cbc6f267598d76d146046b371566d7d8621
pekado <peka.salas@gmail.com>
2021-08-03 16:44:15 -0300

    fav

--- server/db.js
+++ server/db.js
@@ -3,9 +3,9 @@ const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(error => {
   // perform actions on the collection object
+  console.log(uri);
   if (error) return console.error(error);
   var database = client.db("mydatabase");
-  console.log(uri);
   client.close();
 });
 
